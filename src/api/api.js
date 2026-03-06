0c3447dc63540479e244a7452964a696240620ab
KANIYAKM <kaniyakodes@gmail.com>
2026-03-06 00:10:31 -0500

    api update

--- src/api/api.js
+++ src/api/api.js
@@ -11,7 +11,7 @@ import axios from "axios";
 console.log("API BASE URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+baseURL: `${import.meta.env.VITE_API_URL}/api` || 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
